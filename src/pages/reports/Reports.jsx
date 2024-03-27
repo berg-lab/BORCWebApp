270ab10c5df6f696399bee426f353e5356ff8ac2
arjunbky <arjunbky@gmail.com>
2024-03-27 08:40:15 -0500

    vercel deployment test

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -3,33 +3,33 @@ import Chart from 'chart.js/auto';
 import './reports.css'; // Import CSS file
 
 const Reports = () => {
+  const [nodeId, setNodeId] = useState('');
   const [reports, setReportsData] = useState(null);
   const [chart, setChart] = useState(null);
+  const [batteryChart, setBatteryChart] = useState(null);
+
+  const fetchData = async (nodeId) => {
+    try {
+      const response = await fetch(`http://192.168.0.9:5000/thermal/${nodeId}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setReportsData(data.results[0].series[0].values);
+    } catch (error) {
+      console.error('Error fetching thermal data:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://192.168.0.9:5000/thermal/20');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        setReportsData(data.results[0].series[0].values);
-      } catch (error) {
-        console.error('Error fetching thermal data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+    fetchData(nodeId);
+  }, [nodeId]);
 
   useEffect(() => {
-    if (reports) {
-      if (chart) {
-        updateChart();
-      } else {
-        renderChart();
-      }
+    if (reports && !chart) {
+      renderChart();
+    } else if (chart) {
+      updateChart();
     }
   }, [reports]);
 
@@ -63,9 +63,8 @@ const Reports = () => {
           y: {
             beginAtZero: true,
             ticks: {
-              stepSize: 5,     // Set the step size of the axis to 5 units
+              stepSize: 5,
             },
-
           },
         },
       },
@@ -85,10 +84,63 @@ const Reports = () => {
     chart.update();
   };
 
+  useEffect(() => {
+    if (reports && !batteryChart) {
+      renderBatteryChart();
+    }
+  }, [reports]);
+
+  const renderBatteryChart = () => {
+    const ctx = document.getElementById('batteryChart').getContext('2d');
+    const labels = reports.map(([time]) => new Date(time).toLocaleString());
+    const batteries = reports.map(([, , , battery]) => battery / 1000);
+
+    const newChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Battery',
+            data: batteries,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1,
+            },
+          },
+        },
+      },
+    });
+
+    setBatteryChart(newChart);
+  };
+
+  const handleSearch = () => {
+    fetchData(nodeId);
+  };
+
   return (
     <div className="reports-container">
       <h1 className="reports-title">Thermal Data</h1>
+      <div className="searchBar">
+        <input
+          type="text"
+          value={nodeId}
+          onChange={(e) => setNodeId(e.target.value)}
+          placeholder="Enter Node ID"
+          className="searchInput"
+        />
+      </div>
       <canvas id="thermalChart" width="800" height="400"></canvas>
+      <canvas id="batteryChart" width="400" height="200"></canvas>
     </div>
   );
 };
